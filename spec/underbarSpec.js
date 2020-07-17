5c72f32964d7aa1d73913756dd3a0ca45b10a111
PatrickJS <github@gdi2290.com>
2020-07-16 23:13:35 -0700

    Update underbarSpec.js

--- spec/underbarSpec.js
+++ spec/underbarSpec.js
@@ -1,47 +1,70 @@
 var returnArguments = function(){ return arguments; };
 
+  var checkForNativeMethods = function(runUnderbarFunction) {
+    it('should not use the native version of any underbar methods in its implementation', function() {
+      // These spies are set up in testSupport.js
+      runUnderbarFunction();
+      expect(Array.prototype.map.called).to.equal(false);
+      expect(Array.prototype.indexOf.called).to.equal(false);
+      expect(Array.prototype.forEach.called).to.equal(false);
+      expect(Array.prototype.filter.called).to.equal(false);
+      expect(Array.prototype.reduce.called).to.equal(false);
+      expect(Array.prototype.every.called).to.equal(false);
+      expect(Array.prototype.some.called).to.equal(false);
+    });
+  };
 
-describe("first", function() {
-  it("should be able to pull out the first element of an array", function() {
-    expect(_.first([1,2,3])).to.equal(1);
-  });
+    describe('identity', function() {
 
-  it("should be able to accept a user-defined index", function() {
-    expect(_.first([1,2,3], 0)).to.eql([]);
-    expect(_.first([1,2,3], 2)).to.eql([1, 2]);
-    expect(_.first([1,2,3], 5)).to.eql([1, 2, 3]);
-  });
+      it('should return whatever value is passed into it', function() {
+        var uniqueObject = {};
+        expect(_.identity(1)).to.equal(1);
+        expect(_.identity('string')).to.equal('string');
+        expect(_.identity(false)).to.be.false;
+        expect(_.identity(uniqueObject)).to.equal(uniqueObject);
+      });
+    });
 
-  it("should work on an arguments object", function() {
-    var args = returnArguments(1,2,3);
-    expect(_.first(args, 2)).to.eql([1,2]);
-  });
+    describe('first', function() {
 
-});
+      it('should be able to pull out the first element of an array', function() {
+        expect(_.first([1, 2, 3])).to.equal(1);
+      });
 
-describe("last", function() {
-  it("should pull the last element from an array", function() {
-    expect(_.last([1,2,3])).to.equal(3);
-  });
+      it('should accept an index argument', function() {
+        expect(_.first([1, 2, 3], 2)).to.eql([1, 2]);
+      });
 
-  it("should accept an index argument", function() {
-    expect(_.last([1,2,3], 2)).to.eql([2, 3]);
-  });
+      it('should return empty array if zero is passed in as the index', function() {
+        // There is a very important difference between `equal` and `eql`
+        // Can you discover what it is?
+        expect(_.first([1, 2, 3], 0)).to.eql([]);
+      });
 
-  it("should return nothing if zero is passed in as the index", function() {
-    expect(_.last([1,2,3], 0)).to.eql([]);
-  });
+      it('should return all the array\'s elements if the index argument is larger than the length of the array', function() {
+        expect(_.first([1, 2, 3], 5)).to.eql([1, 2, 3]);
+      });
+    });
 
-  it("should return all the array's elements if the index argument is larger than the length of the array", function() {
-    expect(_.last([1,2,3], 5)).to.eql([1, 2, 3]);
-  });
+    describe('last', function() {
 
-  it("should work on an arguments object", function() {
-    var args = returnArguments(1, 2, 3, 4);
-    expect(_.last(args, 2)).to.eql([3, 4]);
-  });
+      it('should pull the last element from an array', function() {
+        expect(_.last([1, 2, 3])).to.equal(3);
+      });
 
-});
+      it('should accept an index argument', function() {
+        expect(_.last([1, 2, 3], 2)).to.eql([2, 3]);
+      });
+
+      it('should return empty array if zero is passed in as the index', function() {
+        expect(_.last([1, 2, 3], 0)).to.eql([]);
+      });
+
+      it('should return all the array\'s elements if the index argument is larger than the length of the array', function() {
+        expect(_.last([1, 2, 3], 5)).to.eql([1, 2, 3]);
+      });
+    });
+
 
 
 /*
